663d35b2fee7d572f4c892d4b3a9a8d96e56799c
Dmytro Vasyliev <dvasyliev.dev@gmail.com>
2022-07-07 07:58:42 +0200

    clean up, added plugins folder

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,4 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
-
-Vue.use(Vuex)
-
-export default new Vuex.Store({
+export default {
   state: {
   },
   getters: {
@@ -14,4 +9,4 @@ export default new Vuex.Store({
   },
   modules: {
   }
-})
+}
